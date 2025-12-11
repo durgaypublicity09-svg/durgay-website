4be0265ab7d50caf94861b10405dc7493a5ab987
durgaypublicity09-svg <durgay.publicity09@gmail.com>
2025-12-11 16:56:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+body {
+    font-family: Arial;
+    background: #f4f4f4;
+    padding: 20px;
+}
+
+#product-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+}
+
+.product {
+    background: white;
+    padding: 15px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px #ccc;
+    text-align: center;
+}
+
+.product img {
+    width: 100%;
+    border-radius: 8px;
+}
